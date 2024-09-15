b3fe46f0a0fa142d12afcac3e71220c5356a16ae
Haddad Sarah <haddads686@gmail.com>
2024-09-15 15:25:28 +0100

    -

--- data/code0-2.py
+++ data/code0-2.py
@@ -2,16 +2,13 @@ import os
 import csv
 
 # Define the folder containing the images
-folder_path = './data'
+folder_path = './'
 
 # Define the name of the CSV file
-csv_file = 'new.csv'
+csv_file = 'valid.csv'
 
 # Get a list of all image files in the folder (change if you have other formats)
 images = [f for f in os.listdir(folder_path) if f.endswith('.png')]
-
-# Sort the images alphabetically
-images.sort()
 
 # Create and write to the CSV file
 with open(csv_file, mode='w', newline='') as file:
@@ -22,6 +19,6 @@ with open(csv_file, mode='w', newline='') as file:
     
     # Write image names and default text "0"
     for image in images:
-        writer.writerow([image, '"'])
+        writer.writerow([image, '0'])
 
 print(f"CSV file '{csv_file}' created successfully!")
