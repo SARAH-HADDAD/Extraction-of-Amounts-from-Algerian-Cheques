59a5795a5137f1f3215ba8d98a4a67c6958483f4
Haddad Sarah <haddads686@gmail.com>
2024-09-15 15:54:01 +0100

    -

---
+++ verification.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from PIL import Image
+import os
+
+
+csv_file = './fr/valid/valid.csv'  
+df = pd.read_csv(csv_file)
+
+
+image_folder = './fr/valid'  
+
+for index, row in df.iterrows():
+    image_path = os.path.join(image_folder, row['file_name'])
+    img = Image.open(image_path)
+    plt.imshow(img)
+    plt.axis('off')  
+    plt.title(f"File: {row['file_name']}\nText: {row['text']}", fontsize=10)
+    plt.show()
