5dc88e4e3a59eb1379a3f3f4b83dc183bf8091a4
Haddad Sarah <haddads686@gmail.com>
2024-09-19 17:42:16 +0100

    updates

---
+++ miaw.py
@@ -0,0 +1,24 @@
+import os
+
+# Define the folder containing the images
+folder_path = './new'
+
+# Get a list of all .png files in the folder
+images = [f for f in os.listdir(folder_path) if f.endswith('.png')]
+
+# Sort the list of images (if needed)
+images.sort()
+
+# Rename the images
+for i, image in enumerate(images, start=39):
+    # Format the new name with 6 digits and .png extension
+    new_name = f"{i:06}.png"
+    
+    # Get the full path of the current and new names
+    old_path = os.path.join(folder_path, image)
+    new_path = os.path.join(folder_path, new_name)
+    
+    # Rename the file
+    os.rename(old_path, new_path)
+
+print("Renaming completed!")
